b789c4af14b47102dfaf7edab6081fd655f64aae
Moataz Alamour <132020912+MoatazAl@users.noreply.github.com>
2025-09-30 14:54:24 +0300

    Time: 3 ms (58.62%), Space: 12.9 MB (98%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        x = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in x:
+                return [x[complement], i]            
+            x[num] = i
